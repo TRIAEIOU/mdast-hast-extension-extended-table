6b5b4aeb7690601e043b8e9dfecf90b416d1b88d
TRIAEIOU <triaeiou@protonmail.com>
2022-07-23 13:34:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+*** ALL CREDIT TO TITUS WORMER (<https://github.com/wooorm>) FOR ORIGINAL CODE ***
+
+## Description
+Extendends mdast/hast to handle delimiter and body only tables markdown ⇔ html. Also includes misc helpers from sub repos.
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { extendedTable as extendedTableSyntax } from './lib/micromark-extension-extended-table/dev';
+export { extendedTableToMarkdown, extendedTableFromMarkdown } from './lib/mdast-util-extended-table';
+export { extendedTable as extendedTableToHast } from './lib/mdast-util-extended-table-to-hast/lib/handlers/table';
+export { extendedTable } from './lib/hast-util-exendeded-table-to-mdast/lib/handlers/table';
