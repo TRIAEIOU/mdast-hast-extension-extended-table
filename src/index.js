799ea3ec5ac46b57b90f9b735985da5ac78bb9a7
TRIAEIOU <94647023+TRIAEIOU@users.noreply.github.com>
2022-08-09 21:52:02 +0200

    Refactor hast-util-to-mdast

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { extendedTable as extendedTableSyntax, extendedTableHtml } from './lib/micromark-extension-extended-table/dev';
 export { extendedTableToMarkdown, extendedTableFromMarkdown } from './lib/mdast-util-extended-table';
 export { extendedTable as extendedTableToHast } from './lib/mdast-util-extended-table-to-hast/lib/handlers/table';
-export { extendedTable } from './lib/hast-util-exendeded-table-to-mdast/lib/handlers/table';
+export { table as extendedTable } from './lib/hast-util-to-mdast/lib/handlers/table';
